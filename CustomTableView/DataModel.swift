7947f7dda88ce6fee37bd83ca699d0075554a4d0
Priyanka <kadam.priyanka1@gmail.com>
2019-11-22 12:44:19 +0530

    Custom Table

--- CustomTableView/DataModel.swift
+++ CustomTableView/DataModel.swift
@@ -8,17 +8,15 @@
 
 import UIKit
 
-class DataModel: UITableViewCell {
+class DataModel {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    var title: String?
+    var city: String?
+    
+    init(title: String?, city: String?) {
+        self.title = title
+        self.city = city
+        
     }
 
 }
